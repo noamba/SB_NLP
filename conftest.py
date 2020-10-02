37009a73fcef97ebad77d74537283bbc241a5586
Noam Bamberger <noamba@gmail.com>
2020-10-02 18:47:38 +0100

    Add phrase matcher fixture

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pytest
 
 from prepare_data import prepare_data
-from setup_phrase_match import get_match_dict
+from setup_phrase_match import get_match_dict, get_phrase_matcher
 
 
 @pytest.fixture
@@ -20,3 +20,8 @@ def prepared_data_fixture(categories_series_fixture):
 @pytest.fixture
 def match_dict_fixture(prepared_data_fixture):
     return get_match_dict(prepared_data_fixture)
+
+
+@pytest.fixture
+def phrase_match_fixture(match_dict_fixture):
+    return get_phrase_matcher(match_dict_fixture)
