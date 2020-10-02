39822398e61e748d8af1c810246501ad77471677
Noam Bamberger <noamba@gmail.com>
2020-10-02 17:29:56 +0100

    Add conftest.py

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import pytest
+
+from prepare_data import prepare_data
+from setup_phrase_match import get_match_dict
+
+
+@pytest.fixture
+def categories_series_fixture():
+    return pd.Series(
+        name="category", data=["Plant-based foods and beverages", "fr:Andouilles"]
+    )
+
+
+@pytest.fixture
+def prepared_data_fixture(categories_series_fixture):
+    return prepare_data(categories_series_fixture)
+
+
+@pytest.fixture
+def match_dict_fixture(prepared_data_fixture):
+    return get_match_dict(prepared_data_fixture)
