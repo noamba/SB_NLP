b41443f97fba82bc2d8df871576c6e737f64c3fe
Noam Bamberger <noamba@gmail.com>
2020-10-03 21:09:11 +0100

    Decouple route configuration and flask app. Will be useful for testing

--- app.py
+++ app.py
@@ -1,44 +1,7 @@
-import json
-
 from flask import Flask
-from flask import request
 
-from nlp.match_categories import get_matched_categories_in_phrase
-from nlp.prepare_data import prepare_data
-from settings import CATEGORIES_FILE, REDUCE_CATEGORY_SET_SIZE, DEBUG
-from nlp.setup_phrase_match import (
-    get_categories,
-    output_categories_df,
-    get_match_dict,
-    get_phrase_matcher,
-)
-
-DEMO_PHRASE = (
-    "Blueberry juices - that`s my favourite. "
-    "But, I also love concentrated apricot juice"
-)
+from routes.routes import configure_routes
 
 app = Flask(__name__)
-
-# set up required objects for matching categories to a phrase
-categories = get_categories(CATEGORIES_FILE, REDUCE_CATEGORY_SET_SIZE)
-prepared_data = prepare_data(categories)
-
-if DEBUG == "Full":
-    output_categories_df(prepared_data)
-
-match_dict = get_match_dict(prepared_data)
-phrase_matcher = get_phrase_matcher(match_dict)
-
-
-@app.route("/")
-def find_categories_in_phrase():
-    phrase = request.args.get("text", default=DEMO_PHRASE, type=str)
-
-    return json.dumps(
-        get_matched_categories_in_phrase(match_dict, phrase_matcher, phrase)
-    )
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+configure_routes(app)
+app.run(debug=True)
---
+++ routes/routes.py
@@ -0,0 +1,39 @@
+import json
+
+from flask import request
+
+from nlp.match_categories import get_matched_categories_in_phrase
+from nlp.prepare_data import prepare_data
+from nlp.setup_phrase_match import (
+    get_categories,
+    output_categories_df,
+    get_match_dict,
+    get_phrase_matcher,
+)
+from settings import CATEGORIES_FILE, REDUCE_CATEGORY_SET_SIZE, DEBUG
+
+DEMO_PHRASE = (
+    "Blueberry juice - that`s my favourite. "
+    "But, I can take concentrated apricot juice on any given day"
+)
+
+
+def configure_routes(app):
+
+    # set up required objects for matching categories to a phrase
+    categories = get_categories(CATEGORIES_FILE, REDUCE_CATEGORY_SET_SIZE)
+    prepared_data = prepare_data(categories)
+
+    if DEBUG == "Full":
+        output_categories_df(prepared_data)
+
+    match_dict = get_match_dict(prepared_data)
+    phrase_matcher = get_phrase_matcher(match_dict)
+
+    @app.route("/")
+    def find_categories_in_phrase():
+        phrase = request.args.get("text", default=DEMO_PHRASE, type=str)
+
+        return json.dumps(
+            get_matched_categories_in_phrase(match_dict, phrase_matcher, phrase)
+        )
