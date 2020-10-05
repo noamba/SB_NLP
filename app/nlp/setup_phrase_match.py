102b258c43a934d6455b8c9e8f3e99486c16079f
Noam Bamberger <noamba@gmail.com>
2020-10-05 19:24:44 +0100

    Add docstrings

--- app/nlp/setup_phrase_match.py
+++ app/nlp/setup_phrase_match.py
@@ -9,6 +9,14 @@ from settings import NLP_ENG
 
 @timeit
 def get_phrase_matcher(match_dict):
+    """Create a Spacy PhraseMatcher object and add match-phrases to it.
+
+    Args:
+        match_dict: {defaultdict} the data from which to create
+            the phrase-matcher
+
+    Retruns: {PhraseMatcher} 
+    """
     matcher = PhraseMatcher(NLP_ENG.vocab, validate=True)
     match_phrases = match_dict.keys()
 
@@ -21,6 +29,19 @@ def get_phrase_matcher(match_dict):
 
 @timeit
 def get_match_dict(categories_df):
+    """Create a dictionary from the
+    cleaned and lemmatized data in categories_df dataframe.
+
+    The structure of the dict will be:
+        {match-phrase : category, ...}
+    And the result will look something like this:
+        {"bluberry juice": "Blueberry juices", "juice": "ar:juice"...}
+
+    Args:
+        categories_df: {pandas DataFrame} includes cleaned + lemmatized strings
+
+    Returns: {dict}
+    """
     match_dict = defaultdict(str)
     # create dict from keys: cleaned + lemmatized, values: original categories
     for index, row in categories_df.iterrows():
@@ -32,6 +53,7 @@ def get_match_dict(categories_df):
 
 
 def output_categories_df(categories_df):
+    """Output contents of dataframe"""
     for index, row in categories_df.iterrows():
         print(
             row.category,
@@ -44,6 +66,13 @@ def output_categories_df(categories_df):
 
 @timeit
 def get_categories(categories_file):
+    """Extract OFF categories from categories_file into a pandas dataframe
+
+    Args:
+        categories_file: {str} path to OFF categories file
+
+    Returns: {pandas DataFrame} dataframe of the data in categories_file
+    """
     categories_file_df = pd.read_csv(categories_file, sep="\t", header=0)
 
     return categories_file_df["category"].astype("string")
