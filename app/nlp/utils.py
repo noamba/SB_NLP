799935b1d1a6084c10418fbce504aec23c2907cd
Noam Bamberger <noamba@gmail.com>
2020-10-05 18:59:41 +0100

    Add docstrings

--- app/nlp/utils.py
+++ app/nlp/utils.py
@@ -5,7 +5,7 @@ from settings import DEBUG
 
 
 def timeit(func):
-
+    """Time func execution if in DEBUG mode. Used as decorator"""
     if not DEBUG:
         return func
 
@@ -27,6 +27,7 @@ def timeit(func):
 
 @timeit
 def save_object_to_disk(object_to_save, path):
+    """Save (pickle) an object to disk"""
     print(f"Saving object to {path}...")
 
     with open(path, "wb") as f:
@@ -37,6 +38,7 @@ def save_object_to_disk(object_to_save, path):
 
 @timeit
 def load_objects_from_disk(path):
+    """Load (unpickle) an object from disk"""
     print(f"Loading object from {path}...")
     with open(path, "rb") as f:
         loaded_object = pickle.load(f)
