f46d4b238016e8218a68dcec9eea1b83eb0bb1a0
Noam Bamberger <noamba@gmail.com>
2020-10-05 12:04:25 +0100

    Improve module doc-string

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
+"""
+To run flask application in dev, either:
+    1) "cd app && export FLASK_APP=main" then "flask run" OR "python main.py"
+        This is as required for the dockerized version which will only copy app/
+    2) debug via pycharm with working dir set as app/
+"""
 from flask import Flask
 
 from views.routes import configure_routes
 from views.helpers import setup_match_objects
 
-"""
-    runs now with
-        1) python app/main.py - as required for the dockerized version
-        2) debug via pycharm
-    It does not run with "export FLASK_APP=app.main  && flask run"
-    
-    Had to remove app. from all imports and
-    change below to app.run(debug=False) to avoid "Restarting with stat" msg
-    and following crash
-"""
 
 print(__name__)
 app = Flask(__name__)
@@ -21,4 +17,5 @@ match_dict, phrase_matcher = setup_match_objects()
 configure_routes(app, match_dict, phrase_matcher)
 
 if __name__ == "__main__":
+    # debug=False to avoid "Restarting with stat" msg and following crash
     app.run(debug=False)
